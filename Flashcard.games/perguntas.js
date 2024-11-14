198fe71bfbc2e6414049853769a3919fa75b88bb
MateCalixto <mateuscalixto291007@gmail.com>
2024-11-14 11:27:48 -0300

    Add files via upload

---
+++ Flashcard.games/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidades',
+    'Qual o jogo mais jogado da história?',
+    'Com a febre dos smartphones e tablets nos últimos anos, várias versões do Tetris foram baixadas. São mais de 425 milhões de download pagos. O jogo que vinha junto com o console da Nintendo vendeu 81,6 milhões de unidades. Atualmente, é o jogo mais jogado em todo o planeta.'
+)
+
+criaCartao(
+    'Videogame',
+    'Qual o primeiro Videogame popular?',
+    'A história dos video games começou um pouco antes do Atari, mas os números deixam bem claro que o Atari 2600 foi o primeiro video game popular. Ele foi o responsável por levar a indústria a um novo patamar, com grandes investimentos no desenvolvimento de jogos.'
+)
+
+criaCartao(
+    'Melhor Videogame',
+    'Qual melhor Videogame?',
+    'O PS5® Slim Edição Digital redefine a experiência de jogo. Este console elimina a necessidade de discos, permitindo que você compre e baixe jogos diretamente da PlayStation®Store através da sua conta da PlayStation Network.'
+)
+
+criaCartao(
+    'Minecraft',
+    'O que é?',
+    'Minecraft é um jogo eletrônico sandbox de sobrevivência criado pelo desenvolvedor sueco Markus "Notch" Persson e posteriormente desenvolvido e publicado pela Mojang Studios, cuja propriedade intelectual foi obtida pela Microsoft em 2014. Lançado inicialmente em maio de 2009 como um projeto em desenvolvimento, seu lançamento completo ocorreu em 18 de novembro de 2011 para Microsoft Windows, macOS, Linux e alguns dispositivos móveis, sendo posteriormente relançado para uma ampla variedade de plataformas. É o jogo eletrônico mais vendido de todos os tempos, vendendo mais de 300 milhões de cópias em todas as plataformas até outubro de 2023, com mais de 140 milhões de jogadores ativos mensalmente.'
+)
